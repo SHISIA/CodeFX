da4d1725645981b2658fbf0815972e09c5feee9d
apple <jamesshisia6@gmail.com>
2022-08-09 00:38:27 +0300

    Full Part A CodeFX Project Repo commit

--- src/main/java/com/codeFx/Controller/QRMain.java
+++ src/main/java/com/codeFx/Controller/QRMain.java
@@ -7,11 +7,11 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class QRMain extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/codeFX/UICode.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(QRMain.class.getResource("/com/codeFX/UICode.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("CodeFX");
         stage.setScene(scene);
